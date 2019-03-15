d3eebb420447ff1467b447337acc1775031d5f61
myllenadias <48135662+myllenadias@users.noreply.github.com>
2019-03-15 15:58:52 -0300

    Delete estudodirigido6.php

--- estudodirigido6.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    print "Digite uma temperatura celsius: ";
-    $temperatura_celsius = fgets (STDIN);
-
-    $temperatura_fahrenheit = 9* $temperatura_celsius/5 + 32;
-
-    print " A temperatura $temperatura_celsius em Celsius convertida para Fahrenheit fica $temperatura_fahrenheit °F ";
-
